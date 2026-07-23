16a033879fe32a196de18a08d004047e3c94b01f
10coolgame-webder <10coolgame@gmail.com>
2026-07-23 02:01:15 +0000

    Implement interactive positioning of parentheses in main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello, World!")
+
+left = 20
+right = 20
+while True:
+    print( " " * left + "()" + " " * right)
+    if input() == "a":
+        left -= 1
+        right += 1
+    elif input() == "d":
+        left += 1
+        right -= 1
